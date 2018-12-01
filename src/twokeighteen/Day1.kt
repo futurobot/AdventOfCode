ac7e70e69ce2cd189bf164c8bc1f60f10d2f8487
Alexey Kalyaganov <aa.kalyaganov@yandex.ru>
2018-12-01 07:09:56 +0300

    Init

---
+++ src/twokeighteen/Day1.kt
@@ -0,0 +1,20 @@
+package twokeighteen
+
+class Day1 {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            System.out.println("Day 1. Puzzle 1")
+
+            System.out.println("Day 2. Puzzle 2")
+        }
+
+        fun solvePuzzle1(captcha: String): String {
+            return ""
+        }
+
+        fun solvePuzzle2(captcha: String): String {
+            return ""
+        }
+    }
+}
